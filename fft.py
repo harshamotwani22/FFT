ed07e02e0f5d804bf7e7d37c6768d6eb4c0acb3d
harshamotwani22 <harsha.motwani22@gmail.com>
2017-05-24 17:18:34 +0530

    Add files via upload

---
+++ fft.py
@@ -0,0 +1,38 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+from PIL import Image
+
+img = cv2.imread('derotate.jpg',0)
+
+img_float32 = np.float32(img)
+
+dft = cv2.dft(img_float32, flags = cv2.DFT_COMPLEX_OUTPUT)
+dft_shift = np.fft.fftshift(dft)
+magnitude_spectrum = 20*np.log(cv2.magnitude(dft_shift[:,:,0],dft_shift[:,:,1]))
+plt.subplot(121),plt.imshow(img, cmap = 'gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
+plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+plt.show() 
+cv2.imwrite("ms.jpg",magnitude_spectrum)
+
+rows, cols = img.shape
+crow, ccol = rows/2 , cols/2     # center
+
+# create a mask first, center square is 1, remaining all zeros
+mask = np.zeros((rows, cols, 2), np.uint8)
+mask[crow-30:crow+30, ccol-30:ccol+30] = 1
+
+# apply mask and inverse DFT
+fshift = dft_shift*mask
+f_ishift = np.fft.ifftshift(fshift)
+img_back = cv2.idft(f_ishift)
+img_back = cv2.magnitude(img_back[:,:,0],img_back[:,:,1])
+cv2.imwrite("ib1.jpg",img_back);
+plt.subplot(121),plt.imshow(img, cmap = 'gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(img_back, cmap = 'gray')
+plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+plt.show()
+cv2.imwrite("ib.jpg",img_back)           
